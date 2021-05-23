5e77c25419d767d52e5f13b90e7888219a30e722
takegami <tkgm15h@gmail.com>
2021-05-23 09:31:00 +0900

    backend: use async and remove dupulicated index in boot script

--- backend/server/boot/02-load-mongo-indexes.ts
+++ backend/server/boot/02-load-mongo-indexes.ts
@@ -8,15 +8,15 @@ module.exports = (app: any) => {
     if (appSetting && appSetting.value === false) {
       const ds = app.dataSources.mongodb;
       // Update indexes for all models
-      ds.connector.connect((err: any, db: any) => {
-        db.collection('AccessToken').createIndexes([
+      ds.connector.connect(async (err: any, db: any) => {
+        await db.collection('AccessToken').createIndexes([
           {key: {ttl: -1}, name: 'ttl'},
           {key: {created: -1}, name: 'created'},
           {key: {userId: 1}, name: 'userId'}
         ]);
         console.log('Updated index for AccessToken');
 
-        db.collection('Alert').createIndexes([
+        await db.collection('Alert').createIndexes([
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {active: 1}, name: 'active'},
           {key: {triggeredAt: -1}, name: 'triggeredAt'},
@@ -26,7 +26,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Alert');
 
-        db.collection('AlertHistory').createIndexes([
+        await db.collection('AlertHistory').createIndexes([
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {triggeredAt: -1}, name: 'triggeredAt'},
           {key: {createdAt: -1}, name: 'createdAt'},
@@ -35,7 +35,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for AlertHistory');
 
-        db.collection('Beacon').createIndexes([
+        await db.collection('Beacon').createIndexes([
           {key: {type: 1}, name: 'type'},
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
@@ -44,7 +44,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Beacon');
 
-        db.collection('Category').createIndexes([
+        await db.collection('Category').createIndexes([
           {key: {type: 1}, name: 'type'},
           {key: {createdAt: -1}, name: 'createdAt'},
           {key: {updatedAt: -1}, name: 'updatedAt'},
@@ -52,7 +52,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Category');
 
-        db.collection('Connector').createIndexes([
+        await db.collection('Connector').createIndexes([
           {key: {type: 1}, name: 'type'},
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
@@ -61,7 +61,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Connector');
 
-        db.collection('Dashboard').createIndexes([
+        await db.collection('Dashboard').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
           {key: {updatedAt: -1}, name: 'updatedAt'},
@@ -69,7 +69,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Dashboard');
 
-        db.collection('Device').createIndexes([
+        await db.collection('Device').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {successRate: 1}, name: 'successRate'},
           {key: {locatedAt: -1}, name: 'locatedAt'},
@@ -83,7 +83,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Device');
 
-        db.collection('Geoloc').createIndexes([
+        await db.collection('Geoloc').createIndexes([
           {key: {type: 1}, name: 'type'},
           {key: {accuracy: -1}, name: 'accuracy'},
           {key: {createdAt: -1}, name: 'createdAt'},
@@ -94,7 +94,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Geoloc');
 
-        db.collection('Message').createIndexes([
+        await db.collection('Message').createIndexes([
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {time: -1}, name: 'time'},
           {key: {seqNumber: -1}, name: 'seqNumber'},
@@ -106,7 +106,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Message');
 
-        db.collection('Organization').createIndexes([
+        await db.collection('Organization').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
           {key: {updatedAt: -1}, name: 'updatedAt'},
@@ -114,21 +114,21 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Organization');
 
-        db.collection('OrganizationCategory').createIndexes([
+        await db.collection('OrganizationCategory').createIndexes([
           {key: {categoryId: 1}, name: 'categoryId'},
           {key: {organizationId: 1}, name: 'organizationId'},
           {key: {createdAt: -1}, name: 'createdAt'},
         ]);
         console.log('Updated index for OrganizationCategory');
 
-        db.collection('OrganizationDevice').createIndexes([
+        await db.collection('OrganizationDevice').createIndexes([
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {organizationId: 1}, name: 'organizationId'},
           {key: {createdAt: -1}, name: 'createdAt'}
         ]);
         console.log('Updated index for OrganizationDevice');
 
-        db.collection('OrganizationMessage').createIndexes([
+        await db.collection('OrganizationMessage').createIndexes([
           {key: {messageId: 1}, name: 'messageId'},
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {organizationId: 1}, name: 'organizationId'},
@@ -136,36 +136,30 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for OrganizationMessage');
 
-        db.collection('OrganizationAlert').createIndexes([
-          {key: {messageId: 1}, name: 'alertId'},
+        await db.collection('OrganizationAlert').createIndexes([
+          {key: {alertId: 1}, name: 'alertId'},
+          {key: {messageId: 1}, name: 'messageId'},
           {key: {deviceId: 1}, name: 'deviceId'},
           {key: {organizationId: 1}, name: 'organizationId'},
-          {key: {createdAt: -1}, name: 'createdAt'}
+          {key: {createdAt: -1}, name: 'createdAt'},
         ]);
         console.log('Updated index for OrganizationAlert');
 
-        db.collection('OrganizationGeoloc').createIndexes([
+        await db.collection('OrganizationGeoloc').createIndexes([
           {key: {geolocId: 1}, name: 'geolocId'},
           {key: {organizationId: 1}, name: 'organizationId'},
           {key: {createdAt: -1}, name: 'createdAt'},
         ]);
         console.log('Updated index for OrganizationGeoloc');
 
-        db.collection('OrganizationAlert').createIndexes([
-          {key: {alertId: 1}, name: 'alertId'},
-          {key: {organizationId: 1}, name: 'organizationId'},
-          {key: {createdAt: -1}, name: 'createdAt'},
-        ]);
-        console.log('Updated index for OrganizationAlert');
-
-        db.collection('Organizationuser').createIndexes([
+        await db.collection('Organizationuser').createIndexes([
           {key: {organizationId: 1}, name: 'organizationId'},
           {key: {userId: 1}, name: 'userId'},
           {key: {createdAt: -1}, name: 'createdAt'},
         ]);
         console.log('Updated index for Organizationuser');
 
-        db.collection('Parser').createIndexes([
+        await db.collection('Parser').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
           {key: {updatedAt: -1}, name: 'updatedAt'},
@@ -173,21 +167,21 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Parser');
 
-        db.collection('Role').createIndexes([
+        await db.collection('Role').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {created: -1}, name: 'created'},
           {key: {modified: -1}, name: 'modified'}
         ]);
         console.log('Updated index for Role');
 
-        db.collection('RoleMapping').createIndexes([
+        await db.collection('RoleMapping').createIndexes([
           {key: {principalType: 1}, name: 'principalType'},
           {key: {principalId: 1}, name: 'principalId'},
           {key: {roleId: 1}, name: 'roleId'}
         ]);
         console.log('Updated index for RoleMapping');
 
-        db.collection('Widget').createIndexes([
+        await db.collection('Widget').createIndexes([
           {key: {name: 1}, name: 'name'},
           {key: {createdAt: -1}, name: 'createdAt'},
           {key: {updatedAt: -1}, name: 'updatedAt'},
@@ -196,7 +190,7 @@ module.exports = (app: any) => {
         ]);
         console.log('Updated index for Widget');
 
-        db.collection('user').createIndexes([
+        await db.collection('user').createIndexes([
           {key: {email: 1}, name: 'email'},
           {key: {username: 1}, name: 'username'},
           {key: {connected: 1}, name: 'connected'},
@@ -208,7 +202,7 @@ module.exports = (app: any) => {
         console.log('Updated index for user');
 
         // Disconnect from datasource
-        ds.connector.disconnect();
+        await ds.connector.disconnect();
       });
 
       appSetting.updateAttribute('value', true, (err: any, appSettingUpdated: any) => {
