8dd578b11200bfb9c47800e27729e748040e8d75
takegami <tkgm15h@gmail.com>
2021-05-30 19:57:55 +0900

    frontend: add atlas native

---
+++ frontend/scripts/test_post_uplink.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import time
+
+headers = {
+    "Authorization": "0BiVPD5A5CQYvioB0CH0DkyzPg587qmkVdyIaY65TmLeTVXNVQARc2WSn0fQZPdm",
+    "Content-Type": "application/json",
+}
+
+payload = {
+    "deviceId": "00000001",
+    "time": int(time.time()),
+    "seqNumber": time.time() % 4096,
+    "data": "fe0500090f423eb90b43"
+}
+
+r = requests.post(
+        "http://localhost:3000/api/Messages/sigfox",
+        headers=headers,
+        data=json.dumps(payload))
+
+print(r)
