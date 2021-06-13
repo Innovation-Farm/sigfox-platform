05bcf57b00da1766fd4c30c6862c9229118c4ee4
takegami <tkgm15h@gmail.com>
2021-06-13 15:26:54 +0900

    frontend: clear token when invalid access token

--- frontend/src/app/shared/sdk/services/core/error.service.ts
+++ frontend/src/app/shared/sdk/services/core/error.service.ts
@@ -1,13 +1,24 @@
 /* tslint:disable */
-import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { LoopBackAuth } from './auth.service';
 import { Observable, throwError } from 'rxjs';
+
 /**
  * Default error handler
  */
 @Injectable()
 export class ErrorHandler {
+  constructor(
+    @Inject(LoopBackAuth) protected auth: LoopBackAuth
+  ) {
+  };
+
   public handleError(errorResponse: HttpErrorResponse): Observable<never> {
+    if (errorResponse.status == 401) {
+      console.warn("user token is expired.");
+      this.auth.clear();
+    }
     return throwError(errorResponse.error.error || 'Server error');
   }
 }
